61039a634ba5eea6f998f12219d127de1fc3a740
Reehl, Sarah <sarah.reehl@pnnl.gov>
2016-07-19 13:11:42 -0700

    Initial commit of plotting themes and dependency sourcing

---
+++ install_and_source_dependencies.R
@@ -0,0 +1,39 @@
+InstalledPackage <- function(package) 
+{
+  available <- suppressMessages(suppressWarnings(sapply(package, require, quietly = TRUE, character.only = TRUE, warn.conflicts = FALSE)))
+  missing <- package[!available]
+  if (length(missing) > 0) return(FALSE)
+  return(TRUE)
+}
+CRANChoosen <- function()
+{
+  return(getOption("repos")["CRAN"] != "@CRAN@")
+}
+UsePackage <- function(package, defaultCRANmirror = "http://cran.at.r-project.org") 
+{
+  if(!InstalledPackage(package))
+  {
+    if(!CRANChoosen())
+    {       
+      chooseCRANmirror()
+      if(!CRANChoosen())
+      {
+        options(repos = c(CRAN = defaultCRANmirror))
+      }
+    }
+    
+    suppressMessages(suppressWarnings(install.packages(package)))
+    if(!InstalledPackage(package)) return(FALSE)
+  }
+  return(TRUE)
+}
+
+libraries <-  c("ggplot2", "shiny", "dplyr", "tuneR", "reshape2", "shinyFiles", "stringr")
+for(library in libraries) 
+{ 
+  if(!UsePackage(library))
+  {
+    stop("Error!", library)
+  }
+}
+
