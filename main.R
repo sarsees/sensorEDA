f337c5ab0766196e1d0c1faa6d78a53869f1db36
Reehl, Sarah <sarah.reehl@pnnl.gov>
2016-08-10 10:33:24 -0700

    Updates to sourcing libraries and running the app

--- main.R
+++ main.R
@@ -1,8 +1,8 @@
-library(shiny)
-library(shinyFiles)
-
-#Get the main.R directory
-cwd = dirname(sys.frame(1)$ofile)
-      
-#Run the app
-shiny::runApp(cwd)
+#!/usr/bin/env Rscript
+initial.options <- commandArgs(trailingOnly = FALSE)
+file.arg.name <- "--file="
+script.name <- sub(file.arg.name, "", initial.options[grep(file.arg.name, initial.options)])
+script.basename <- dirname(script.name)
+other.name <- paste(sep="/", script.basename, "launch_app.R")
+print(paste("Sourcing",other.name,"from",script.name))
+source(other.name)
