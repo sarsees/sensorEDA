4d7c5cd7b8e2e8b7582b307334ea8ae487e71d60
Fortin, Daniel C <daniel.fortin@pnnl.gov>
2016-08-19 10:57:40 -0700

    change ECG processing to take raw differences between the two channels

--- functions/ecg_processing.R
+++ functions/ecg_processing.R
@@ -1,27 +1,10 @@
 
 #' Calculate ECG signal using input from both ECG sensors
 #' 
-#' @param ecg1 object with column names time and volt
-#' @param ecg2 object with column names time and volt
+#' @param ecg object with column names time and volt1 and volt2
 ecg_processing <- function(ecg){
-  Hz <- 60 # assuming sampling is at 60Hz
-  min_t <- max(min(ecg$time), min(ecg$time))
-  max_t <- min(max(ecg$time), max(ecg$time))
-  
-  # create vector of time points for evaluating ECG curves
-  #ts <- seq(from = min_t, to = max_t, length = Hz*(max_t - min_t)) 
-  ts <- ecg$time 
-  
-  # fit each curve with a linear interpolating function
-  ecg1_fit <- approxfun(ecg$time, ecg$volt1)
-  ecg2_fit <- approxfun(ecg$time, ecg$volt2)
-  
-  # compute fitted values for each curve
-  fit_ecg1 <- ecg1_fit(ts)
-  fit_ecg2 <- ecg2_fit(ts)
-  ecg_diff <- fit_ecg1 - fit_ecg2
-  
-  res <- data.frame(time = ts, ecg1 = fit_ecg1, ecg2 = fit_ecg2, ecg_diff = ecg_diff)
+  ecg_diff <- ecg$volt1 - ecg$volt2
+  res <- data.frame(time = ecg$time, ecg1 = ecg$volt1, ecg2 = ecg$volt2, ecg_diff = ecg_diff)
   return(res)
 }
 
